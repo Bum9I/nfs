a382df2dfe4ce3808c75ad58756fc02bf6f291f9
Bum9I <133780406+Bum9I@users.noreply.github.com>
2023-05-20 08:42:34 +0300

    first commit

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    
+    void Update()
+    {
+        var position = transform.position;
+        var step = _speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            position.x += step;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            position.x -= step;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            position.z += step;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            position.z -= step;
+        }
+        transform.position = position;
+    }
+}
